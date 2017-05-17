47112e72c431d73f40e2cfedb1cc21acabae2ad8
Chavit Thawanapong <chavit.t@th.zalora.com>
2017-05-17 07:24:24 +0700

    - Change StyleableButton implementation to allow more customization

--- styleable-views/src/main/java/com/github/cthawanapong/view/StyleableButton.java
+++ styleable-views/src/main/java/com/github/cthawanapong/view/StyleableButton.java
@@ -35,8 +35,12 @@ public class StyleableButton extends FrameLayout implements View.OnClickListener
     private static final String ARG_TEXT_COLOR = "ARG_TEXT_COLOR";
     private static final String ARG_DRAWABLE_PADDING = "ARG_DRAWABLE_PADDING";
     private static final String ARG_HORIZONTAL_PADDING = "ARG_HORIZONTAL_PADDING";
+    private static final String ARG_BORDER_COLOR = "ARG_BORDER_COLOR";
+    private static final String ARG_CORNER_RADIUS = "ARG_CORNER_RADIUS";
+    private static final String ARG_ELEVATION = "ARG_ELEVATION";
 
     //View Members
+    private StyleableNoPaddingCardView mLayoutCard;
     private ViewGroup mLayoutButton;
     private ViewGroup mLayoutText;
     private ViewGroup mLayoutWrapper;
@@ -51,6 +55,9 @@ public class StyleableButton extends FrameLayout implements View.OnClickListener
     private Drawable mLeftIcon;
     private int mButtonDrawablePadding;
     private int mButtonHorizontalPadding;
+    private int mBorderColor;
+    private int mCornerRadius;
+    private int mElevation;
     private OnClickListener mListener;
 
     public StyleableButton(Context context) {
@@ -84,12 +91,13 @@ public class StyleableButton extends FrameLayout implements View.OnClickListener
     }
 
     private void initInstance(Context context, AttributeSet attrs) {
+        mLayoutCard = (StyleableNoPaddingCardView) findViewById(R.id.layout_border);
         mLayoutButton = (ViewGroup) findViewById(R.id.layout_button);
         mLayoutText = (ViewGroup) findViewById(R.id.layout_text);
         mLayoutWrapper = (ViewGroup) findViewById(R.id.layout_wrapper);
         mTextView = (StyleableTextView) findViewById(R.id.txt_view);
-
         mButtonText = "";
+
         if (attrs != null) {
             TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.StyleableButton, 0, 0);
             try {
@@ -101,6 +109,9 @@ public class StyleableButton extends FrameLayout implements View.OnClickListener
                 mLeftIcon = ta.getDrawable(R.styleable.StyleableButton_buttonLeftDrawable);
                 mButtonDrawablePadding = ta.getDimensionPixelSize(R.styleable.StyleableButton_buttonDrawablePadding, 0);
                 mButtonHorizontalPadding = ta.getDimensionPixelSize(R.styleable.StyleableButton_buttonHorizontalPadding, 0);
+                mBorderColor = ta.getColor(R.styleable.StyleableButton_buttonBorderColor, Color.BLACK);
+                mCornerRadius = ta.getDimensionPixelSize(R.styleable.StyleableButton_buttonCornerRadius, 0);
+                mElevation = ta.getDimensionPixelSize(R.styleable.StyleableButton_buttonElevation, 0);
             } finally {
                 ta.recycle();
             }
@@ -112,6 +123,9 @@ public class StyleableButton extends FrameLayout implements View.OnClickListener
         mTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSize);
         mTextView.setCompoundDrawablesWithIntrinsicBounds(mLeftIcon, null, null, null);
         mTextView.setCompoundDrawablePadding(mButtonDrawablePadding);
+        mLayoutCard.setCardBackgroundColor(mBorderColor);
+        mLayoutCard.setRadius(mCornerRadius);
+        mLayoutCard.setCardElevation(mElevation);
         mLayoutWrapper.setPadding(mButtonHorizontalPadding, 0, mButtonHorizontalPadding, 0);
         mLayoutWrapper.setOnClickListener(this);
         setDisplayButtonText();
@@ -155,6 +169,9 @@ public class StyleableButton extends FrameLayout implements View.OnClickListener
         bundle.putInt(ARG_TEXT_COLOR, mTextColor);
         bundle.putInt(ARG_DRAWABLE_PADDING, mButtonDrawablePadding);
         bundle.putInt(ARG_HORIZONTAL_PADDING, mButtonHorizontalPadding);
+        bundle.putInt(ARG_BORDER_COLOR, mBorderColor);
+        bundle.putInt(ARG_CORNER_RADIUS, mCornerRadius);
+        bundle.putInt(ARG_ELEVATION, mElevation);
 
         //Save it to Parcelable
         BundleSavedState ss = new BundleSavedState(superState);
@@ -193,6 +210,9 @@ public class StyleableButton extends FrameLayout implements View.OnClickListener
         this.mTextColor = bundle.getInt(ARG_TEXT_COLOR);
         this.mButtonDrawablePadding = bundle.getInt(ARG_DRAWABLE_PADDING);
         this.mButtonHorizontalPadding = bundle.getInt(ARG_HORIZONTAL_PADDING);
+        this.mBorderColor = bundle.getInt(ARG_BORDER_COLOR);
+        this.mCornerRadius = bundle.getInt(ARG_CORNER_RADIUS);
+        this.mElevation = bundle.getInt(ARG_ELEVATION);
     }
 
     public void setListener(OnClickListener mListener) {
@@ -212,6 +232,11 @@ public class StyleableButton extends FrameLayout implements View.OnClickListener
     public void setBackgroundColor(int color) {
         mBackgroundColor = color;
         mLayoutText.setBackgroundColor(mBackgroundColor);
+    }
+
+    public void setBorderColor(int color) {
+        mBorderColor = color;
+        mLayoutCard.setCardBackgroundColor(mBorderColor);
     }
 
     @Override
